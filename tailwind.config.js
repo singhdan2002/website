be9a092f88ccd4fa714ab03ab0817663c2201219
singhdan2002 <89182111+singhdan2002@users.noreply.github.com>
2022-01-28 19:26:35 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      
+      extend: {
+        backgroundColor: ["active"],
+        fontFamily: {
+          'montserrat' : ['"Montserrat"', 'sans-serif']
+        },
+      },
+    },
+    plugins: [],
+  }
