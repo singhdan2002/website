5e3e88438ebaaf9b50f959f0aa4ea5c29a39ec3d
Danishjeet Singh <danishjeetsingh@Danishjeets-MacBook-Pro.local>
2022-03-22 20:20:38 -0400

    mobile nav started

---
+++ src/script.js
@@ -0,0 +1,8 @@
+	// Grab HTML Elements
+	const btn = document.querySelector(".mobile-menu-button");
+	const menu = document.querySelector(".mobile-menu");
+
+	// Add Event Listeners
+	btn.addEventListener("click", () => {
+	    menu.classList.toggle("hidden");
+    });
