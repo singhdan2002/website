f390f1831d0136488ad4ac7a2146424384a08fa6
Danishjeet Singh <danishjeetsingh@Danishjeets-MacBook-Pro.local>
2022-03-22 22:09:11 -0400

    old nav is back

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 	// Grab HTML Elements
 	const btn = document.querySelector(".mobile-menu-button");
-	const menu = document.querySelector(".mobile-menu");
+	const menu = document.querySelector("#mobile-menu");
 
 	// Add Event Listeners
 	btn.addEventListener("click", () => {
