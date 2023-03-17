9a21f78e61074f3fe0ca9a4eac4141413941cc8f
DanishjeetSingh <danishjeetsingh@gmail.com>
2023-03-17 00:30:01 -0400

    made background and symbol, color changes

--- creative_coding/matrix_rain/script.js
+++ creative_coding/matrix_rain/script.js
@@ -1,12 +1,16 @@
+// Get the canvas element and create a 2D drawing context
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 
+// Set the canvas width and height to fill the window size
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// Create a Symbol class with properties and a draw method
 class Symbol{
     constructor(x,y,fontSize,canvasHeight){
-        this.characters = 'アァカサタナハマヤャラワガザダバパイィキシチニヒミリヰギジヂビピウゥクスツヌフムユュルグズブヅプエェケセテネヘメレヱゲゼデベペオォコソトノホモヨョロヲゴゾドボポヴッン0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        // Set a string of characters to be displayed randomly
+        this.characters = '♠︎♣︎♥︎♦';
         this.x = x;
         this.y = y;
         this.fontSize = fontSize;
@@ -14,8 +18,11 @@ class Symbol{
         this.canvasHeight = canvasHeight;
     }
     draw(context){
+        // Choose a random character from the string and draw it at the x and y position
         this.text = this.characters.charAt(Math.floor(Math.random() * this.characters.length));
         context.fillText(this.text, this.x * this.fontSize, this.y * this.fontSize);
+        
+        // If the symbol goes beyond the canvas height, reset the position
         if(this.y * this.fontSize > this.canvasHeight && Math.random() > 0.98){
             this.y = 0;
         }else{
@@ -24,6 +31,7 @@ class Symbol{
     }
 }
 
+// Create an Effect class with properties and a method to initialize symbols
 class Effect{
     constructor(canvasWidth, canvasHeight){
         this.canvasWidth = canvasWidth;
@@ -31,16 +39,23 @@ class Effect{
         this.fontSize = 25;
         this.cols = this.canvasWidth/this.fontSize;
         this.symbols = [];
-        this.#init();
-        console.log(this.symbols);
+        this.#init(); // Initialize symbols
     }
-    #init(){
+    #init(){ // Private method to initialize symbols
         for(let i = 0; i< this.cols; i++){
             this.symbols[i] = new Symbol(i,0,this.fontSize,this.canvasHeight);
         }
     }
+    resize(width, height){ // Method to resize the canvas and symbols
+        this.canvasWidth = width;
+        this.canvasHeight = height;
+        this.cols = this.canvasWidth/this.fontSize;
+        this.symbols = [];
+        this.#init(); // Re-initialize symbols
+    }
 }
 
+// Create an instance of Effect with the canvas dimensions
 const effect = new Effect(canvas.width, canvas.height);
 
 let lastTime = 0;
@@ -48,22 +63,33 @@ const fps = 30;
 const nextFrame = 1000/fps;
 let timer = 0;
 
+// The main animation loop function
 function animate(timeStamp){
     const deltaTime = timeStamp - lastTime;
     lastTime = timeStamp;
     if(timer > nextFrame){
+        // Set a black background with low opacity to create a trailing effect
         ctx.fillStyle = 'rgba(0,0,0,0.05)'
         ctx.textAlign = 'center';
         ctx.fillRect(0,0, canvas.width, canvas.height);
-        ctx.fillStyle = '#0aff0a';
-        ctx.font = effect.fontSize + 'px monospace';
+        
+        // Set the font color and size, and draw each symbol
+        ctx.fillStyle = '#FF6B6B';
+        ctx.font = `${effect.fontSize}px monospace`
         effect.symbols.forEach(symbol => symbol.draw(ctx));
         timer = 0
     }else{
         timer+=deltaTime;
     }
     
-    requestAnimationFrame(animate);
+    requestAnimationFrame(animate); // Call the function recursively to create a smooth animation
 }
 
-animate(0);
+animate(0); // Call the function to start the animation
+
+// Add an event listener to resize the canvas and symbols when the window is resized
+window.addEventListener('resize', function(){
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    effect.resize(canvas.width,canvas.height)
+})
